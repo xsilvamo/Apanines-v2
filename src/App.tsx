c7b78a4e2d146a0efd69eb328fb651b714cc2535
Manuxxrsb <Manuxxrsb@gmail.com>
2024-12-11 20:26:46 -0300

    Formatear código y actualizar configuraciones de archivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Navbar from './components/layout/Navbar';
-import Hero from './components/sections/Hero';
-import MainContent from './components/sections/MainContent';
-import Footer from './components/layout/Footer';
+import React from "react";
+import Navbar from "./components/layout/Navbar";
+import Hero from "./components/sections/Hero";
+import MainContent from "./components/sections/MainContent";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
