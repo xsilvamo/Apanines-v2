e478233e3e58d139f5328a1c035e35bbadfe44d6
Manuxxrsb <Manuxxrsb@gmail.com>
2024-12-11 20:31:55 -0300

    Actualizar configuración de Vite para incluir directorio de salida y formatear código

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "dist",
   },
 });
